9d7a04ab1ada1ec78197f0eb08016a9782f7e4e2
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2025-03-09 14:20:40 +0100

    Update CorsConfig.java

--- backend/src/main/java/com/hairdresser/my_spring_project/config/CorsConfig.java
+++ backend/src/main/java/com/hairdresser/my_spring_project/config/CorsConfig.java
@@ -14,7 +14,10 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+            "https://hairsalonbookingapp-java.pawelsobon.pl",
+            "https://www.hairsalonbookingapp-java.pawelsobon.pl"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
